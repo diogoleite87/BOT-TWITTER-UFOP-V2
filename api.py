ccd69a6b42b81cf4b262a3084414b11b886bdaec
Diogo Leite <diogoleite87@gmail.com>
2023-09-13 19:22:05 -0300

    feat: add api client config for twitter

---
+++ api.py
@@ -0,0 +1,12 @@
+import tweepy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = tweepy.Client(
+    consumer_key=os.getenv("API_KEY"),
+    consumer_secret=os.getenv("API_KEY_SECRET"),
+    access_token=os.getenv("ACCESS_TOKEN"),
+    access_token_secret=os.getenv("ACCESS_TOKEN_SECRET")
+)
