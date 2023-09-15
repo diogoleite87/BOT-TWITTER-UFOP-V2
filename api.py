03ae1bba9894e19bb816be2926d19c4b09d21a48
Diogo Leite <diogoleite87@gmail.com>
2023-09-15 03:45:10 -0300

    feat: add client v1.1 fo twitter api

--- api.py
+++ api.py
@@ -4,7 +4,11 @@ import os
 
 load_dotenv()
 
-client = tweepy.Client(
+auth = tweepy.OAuth1UserHandler(os.getenv("API_KEY"), os.getenv("API_KEY_SECRET"))
+auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET"))
+client_v1 = tweepy.API(auth)
+
+client_v2 = tweepy.Client(
     consumer_key=os.getenv("API_KEY"),
     consumer_secret=os.getenv("API_KEY_SECRET"),
     access_token=os.getenv("ACCESS_TOKEN"),
